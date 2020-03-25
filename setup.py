8725b312477da828437e9195605008b07efa0c70
basharatoum <atoumbashar@gmail.com>
2020-03-25 00:40:51 -0500

    fixes

--- setup.py
+++ setup.py
@@ -19,27 +19,25 @@ def no_cythonize(extensions, **_ignore):
         extension.sources[:] = sources
     return extensions
 extensions = [
-    Extension("*",["src/*.pyx"],include_dirs=[numpy.get_include(),os.getcwd()+'/src/']),
-    Extension("*",["src/Gases/*.pyx"],include_dirs=[numpy.get_include(),os.getcwd()+'/src/']),
+    Extension("*",["PyGasMix/*.pyx"],include_dirs=[numpy.get_include(),os.getcwd()+'/PyGasMix/']),
+    Extension("*",["PyGasMix/Gases/*.pyx"],include_dirs=[numpy.get_include(),os.getcwd()+'/PyGasMix/']),
 ]
-#setup(ext_modules=cythonize(extensions))
-
 setup(
     setup_requires=[
         'cython>=0.2',
     ],
     zip_safe=False,
     name='PyGasMix',  # Required
-    packages=['src','src/Gases'],
+    packages=['PyGasMix','PyGasMix/Gases'],
 
     version='1.0.0',  # Required
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
 
     install_requires=['numpy==1.16.1','cython'],  # Optional
     package_data={  # Optional
-        'src': ['./src/*.pxd'],
-        'src/Gases':['./src/Gases/gases.npy','./src/Gases/*.pxd','./src/*.pxd']
+        'src': ['./PyGasMix/*.pxd'],
+        'src/Gases':['./PyGasMix/Gases/gases.npy','./PyGasMix/Gases/*.pxd','./PyGasMix/*.pxd']
     },
-    extensions = no_cythonize(extensions),
+    ext_modules = extensions,
     cmdclass={'build_ext': Cython.Build.build_ext},
 )
