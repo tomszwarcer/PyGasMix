c24757817f0e614cb5a19ac12f364d49d755f609
basharatoum <atoumbashar@gmail.com>
2020-03-25 00:16:53 -0500

    fixes

--- setup.py
+++ setup.py
@@ -5,29 +5,28 @@ import Cython
 import numpy
 from io import open
 
-
 extensions = [
     Extension("*",["src/*.pyx"],include_dirs=[numpy.get_include(),os.getcwd()+'/src/']),
     Extension("*",["src/Gases/*.pyx"],include_dirs=[numpy.get_include(),os.getcwd()+'/src/']),
 ]
 #setup(ext_modules=cythonize(extensions))
-extensions = cythonize(extensions)
 
 setup(
     setup_requires=[
-        'cython>=0.2x',
+        'cython>=0.2',
     ],
 
     name='PyGasMix',  # Required
-    packages=['src'],
+    packages=['src','src/Gases'],
 
     version='1.0.0',  # Required
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
 
     install_requires=['numpy==1.16.1','cython'],  # Optional
     package_data={  # Optional
-        'src': ['/src/Gases/gases.npy','/src/*.pxd','/src/Gases/*.pxd'],
+        'src': ['./src/*.pxd'],
+        'src/Gases':['./src/Gases/gases.npy','./src/Gases/*.pxd','./src/*.pxd']
     },
-    extensions=extensions,
+    ext_modules = cythonize(extensions),
     cmdclass={'build_ext': Cython.Build.build_ext},
 )
