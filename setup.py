7cbe7675f2b8e46a53086232698f5e2327db81c7
basharatoum <atoumbashar@gmail.com>
2020-03-24 23:53:09 -0500

    first push

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup, find_packages, Extension
+from Cython.Build import cythonize
+import Cython
+import numpy
+from io import open
+
+
+extensions = [
+    Extension("*",["src/*.pyx"],include_dirs=[numpy.get_include(),os.getcwd()+'/src/']),
+    Extension("*",["src/Gases/*.pyx"],include_dirs=[numpy.get_include(),os.getcwd()+'/src/']),
+]
+#setup(ext_modules=cythonize(extensions))
+
+setup(
+    setup_requires=[
+        'cython>=0.x',
+    ],
+
+    name='PyGasMix',  # Required
+    packages=['src'],
+
+    version='1.0.0',  # Required
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
+
+    install_requires=['numpy==1.16.1','cython'],  # Optional
+    package_data={  # Optional
+        'src': ['/src/Gases/gases.npy','/src/*.pxd','/src/Gases/*.pxd'],
+    },
+    extensions=cythonize(extensions),
+    cmdclass={'build_ext': Cython.Build.build_ext},
+)
---
+++ src/Gases/XENONMERT.pxd
@@ -0,0 +1,3 @@
+from Gas cimport Gas
+
+cdef void Gas61(Gas*object,double AA,double D,double F,double A1,double Lambda,double EV0)
